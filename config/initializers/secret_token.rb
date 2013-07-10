4f0346c7a984477826a175d173ef3ab19f5d78eb
drew.kowalik@gmail.com <drew.kowalik@gmail.com>
2013-07-09 17:23:30 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MemeGenerator::Application.config.secret_token = 'e70f28f2b94fea747c08b0160fbbd8e8fe53839b0d70d49c556d2a582614a933174e1124ff450758da0f79bb7e01e55a1bc6ecd567b7811d4444c3c95f6b36f6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MemeGenerator::Application.config.session_store :cookie_store, key: '_meme_generator_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MemeGenerator::Application.config.session_store :active_record_store
